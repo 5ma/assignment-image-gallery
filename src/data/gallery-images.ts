6e365f48c8f28c35eca9e1488c951cfd6ea27c07
komakine-anna <anna.komakine@relic.co.jp>
2023-11-12 13:08:57 +0900

    🔨 画像データの配列を作成

---
+++ src/data/gallery-images.ts
@@ -0,0 +1,128 @@
+export type GalleryImageItem = {
+  id: number
+  src: string
+  alt: string
+}
+
+export const galleryImages: GalleryImageItem[] = [
+  {
+    id: 1,
+    src: 'pic_016.png',
+    alt: ''
+  },
+  {
+    id: 2,
+    src: 'pic_015.png',
+    alt: ''
+  },
+  {
+    id: 3,
+    src: 'pic_014.png',
+    alt: ''
+  },
+  {
+    id: 4,
+    src: 'pic_013.png',
+    alt: ''
+  },
+  {
+    id: 5,
+    src: 'pic_012.png',
+    alt: ''
+  },
+  {
+    id: 6,
+    src: 'pic_011.png',
+    alt: ''
+  },
+  {
+    id: 7,
+    src: 'pic_010.png',
+    alt: ''
+  },
+  {
+    id: 8,
+    src: 'pic_009.png',
+    alt: ''
+  },
+  {
+    id: 9,
+    src: 'pic_008.png',
+    alt: ''
+  },
+  {
+    id: 10,
+    src: 'pic_007.png',
+    alt: ''
+  },
+  {
+    id: 11,
+    src: 'pic_006.png',
+    alt: ''
+  },
+  {
+    id: 12,
+    src: 'pic_005.png',
+    alt: ''
+  },
+  {
+    id: 13,
+    src: 'pic_004.png',
+    alt: ''
+  },
+  {
+    id: 14,
+    src: 'pic_005.png',
+    alt: ''
+  },
+  {
+    id: 15,
+    src: 'pic_019.png',
+    alt: ''
+  },
+  {
+    id: 16,
+    src: 'pic_018.png',
+    alt: ''
+  },
+  {
+    id: 17,
+    src: 'pic_017.png',
+    alt: ''
+  },
+  {
+    id: 18,
+    src: 'pic_022.png',
+    alt: ''
+  },
+  {
+    id: 19,
+    src: 'pic_021.png',
+    alt: ''
+  },
+  {
+    id: 20,
+    src: 'pic_026.png',
+    alt: ''
+  },
+  {
+    id: 21,
+    src: 'pic_023.png',
+    alt: ''
+  },
+  {
+    id: 22,
+    src: 'pic_025.png',
+    alt: ''
+  },
+  {
+    id: 23,
+    src: 'pic_024.png',
+    alt: ''
+  },
+  {
+    id: 24,
+    src: 'pic_020.png',
+    alt: ''
+  }
+]
