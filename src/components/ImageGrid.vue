c45501501267d833ba0b75c38c0686ed186992d2
komakine-anna <anna.komakine@relic.co.jp>
2023-11-12 13:10:24 +0900

    🚧 Grid viewのhtml / css実装

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>ImageGrid.vue</div>
+  <div class="grid">
+    <div v-for="image in images" :key="image.id" class="grid__item">
+      <img :src="getImagePath(image.src)" :alt="image.alt" loading="lazy" />
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import type { GalleryImageItem } from '@/data/gallery-images'
 
-<style lang="scss" scoped></style>
+export default defineComponent({
+  props: {
+    images: {
+      type: Array as PropType<GalleryImageItem[]>,
+      required: true
+    },
+    id: Number
+  },
+  methods: {
+    getImagePath(fileName: string) {
+      return new URL(`../assets/images/${fileName}`, import.meta.url).href
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/global/media-query' as mq;
+
+.grid {
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+
+  @include mq.pc {
+    gap: 15px;
+  }
+}
+</style>
